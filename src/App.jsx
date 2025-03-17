7e25f4792c50a4866e4233ce5c432b9f1871815c
Kibiwott <bk.kibiwott@gmail.com>
2025-03-17 16:44:28 +0300

    Chenged the project to a movie show app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+
 import './App.css'
-import { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [weather, setWeather] = useState([]);
+    const [show, setShow] = useState('')
+    const [results, setResults] = useState(null)
 
-  async function getWeather() {
-    const response = await fetch('https://goweather.herokuapp.com/weather/Nairobi') 
-    const data = await response.json()
-    console.log(data) 
-  }
 
- 
+async function getProducts(){
+    const res = await fetch(`https://api.tvmaze.com/singlesearch/shows?q= ${show}`)
+    const data = await res.json()
+    console.log(data)
+    setResults(data);
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    getProducts();
+}
+
+
 
 
   return (
-    <div>
-      <button onClick={getWeather}>Get Weather</button>
+    <div className='container'>
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={(e) => setShow(e.target.value)} />
+            <button type='submit'>Search</button>
+        </form>
+        <Show show={results} />
     </div>
   )
 }
@@ -24,3 +38,25 @@ const App = () => {
 export default App
 
 
+
+function Show({show}){
+    return(
+        <div className='output'>
+            <div className="imageWrapper">
+                <img src={show?.image?.medium} alt="Show image" />
+            </div>
+            <h1>{show?.name}</h1>
+            <div className="description">            
+            <h3>Genres: {show?.genres}</h3>
+            <h3>Languages: {show?.language}</h3>
+            <h3>Status: {show?.status}</h3>
+            <h3>Time: {show?.schedule?.time}</h3>
+            <h3>Country: {show?.network.country?.name}</h3>
+            <h3> Type: {show?.type}</h3>
+            </div>
+            <p dangerouslySetInnerHTML={{__html: show?.summary|| 'No summmary'}}>
+
+            </p>
+        </div>
+    )
+}
